fb3c60e1965e184af69dbbc3f4fcf9da52209903
VikramKumar82092 <128372205+VikramKumar82092@users.noreply.github.com>
2026-01-05 05:50:36 +0530

    Refactor server.js to use dynamic port

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/products", require("./routes/productRoutes"));
 app.use("/orders", require("./routes/orderRoutes"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
 
